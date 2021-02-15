7eeac58810f0be92b49de1969d24a0c9aea37726
Hosely5 <tappannagari@gmail.com>
2021-02-15 10:53:52 -0600

    Login and Registration

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
+const passport = require('passport')
+const session = require('express-session')
 
+require('./config/passport')(passport)
+
+const db = require('./config/keys').MongoURI
+mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+	.then(() => console.log("Connected to MongoDB"))
+	.catch((err) => console.log("Error: " + err))
 
 const app = express()
 
@@ -9,12 +17,27 @@ app.use(expressLayouts)
 app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({ extended: false}))
+
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Authentication
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
 	console.log("Server started")
 })
 
+// CSS
 app.use('/', express.static('views'))
 
 app.use('/', require('./routes/index'))
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 
-
 router = express.Router()
+
+const {ensureAuthenticated} = require('../config/auth')
 
 router.get("/", (req,res) => {
 	res.render('welcome')
+})
+
+router.get('/homepage',ensureAuthenticated, (req, res) => {
+	res.render('homepage', {
+		name: req.user.name,
+		email: req.user.email,
+	})
 })
 
 module.exports = router
