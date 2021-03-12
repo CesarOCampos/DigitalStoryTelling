6f2d790fad81efb304eedeb21a9152e285a78df3
Hosely5 <tappannagari@gmail.com>
2021-03-12 10:32:46 -0600

    Front-End

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,17 @@
+// Login, Register, View Videos Page, Watch Video, Save Video Timestamps, Search for Videos, Admin Account
+
+
 const express = require('express')
-
 router = express.Router()
-
 const {ensureAuthenticated} = require('../config/auth')
+
+
 
 router.get("/", (req,res) => {
 	res.render('welcome')
 })
+
+
 
 router.get('/homepage',ensureAuthenticated, (req, res) => {
 	res.render('homepage', {
