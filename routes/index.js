af47f340b19578f42100edde368fe55b4277ad80
Hosely5 <tappannagari@gmail.com>
2021-04-04 00:21:09 -0500

    Front-End Folder

--- routes/index.js
+++ routes/index.js
@@ -52,7 +52,7 @@ router.get('/videos/:videoname', ensureAuthenticated, (req,res) => {
 	console.log(vidstart)
 
 	if(vidname == "video1"){
-		res.render('videopage', {heading: vidname, videolink: `https://www.youtube.com/embed/6FOq4cUdH8k`, time: vidstart})
+		res.render('videopage', {heading: vidname, videolink: `https://www.youtube.com/embed/aHRXXdCpKBk`, time: vidstart})
 	}else if(vidname == "video2"){
 		res.render('videopage', {heading: vidname, videolink: "https://www.youtube.com/embed/mTz0GXj8NN0", time: vidstart})
 	}else if(vidname == "video3"){
