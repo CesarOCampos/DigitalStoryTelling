c5d39bdd99bff6ce8a306066208e696274275bcd
bree-dot <77509576+bree-dot@users.noreply.github.com>
2022-04-01 00:10:47 -0500

    changed pages

---
+++ pages/footer.tsx
@@ -0,0 +1,21 @@
+const footer = () => (
+    <div className="footer">
+    <img src="../media/uh.png" alt="title image" className="uh-image-center"/>
+        <a className="footer-text" href="#top">Go to top of page</a>
+        
+        <a className="Designed-by">Designed by: The Computer Science students of the University of Houston</a>
+        
+        
+        <section>
+    
+        <a className="Contact-Us"> Contact Us @ </a>
+        <a className="Contact info"> njrizk@uh.edu </a>
+        <a className="address"> 4800 Calhoun Rd </a>
+        <a className="add2"> Houston </a>
+        <a className="add3"> Texas, 77004 </a>
+        </section>
+    </div>
+    
+      );
+
+      export default footer;
