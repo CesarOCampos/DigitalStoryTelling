5b441a4163bc1d470a384eeda12bc63856615641
Cesar Campos <40922162+CesarOCampos@users.noreply.github.com>
2022-04-18 23:00:49 -0500

    save

--- models/schema.js
+++ models/schema.js
@@ -1,12 +1,17 @@
 const mongodb = require("mongodb");
+// const mongoose = require("mongoose");
 const { stringify } = require("querystring");
 
 const Schema = mongodb.schema;
 
 const digitalStoryTellingSchema = new Schema({
-  name: string
-  
-
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: {type: String, required: true},
+  username: {type: String},
+  videos: {type: Array},
+  isAdmin: {type: Boolean},
+  myvideos: {type: Array},
 });
 
 const DST = mongodb.model("DigitalStoryTelling", digitalStoryTellingSchema);
--- pages/api/auth/auth.js
+++ pages/api/auth/auth.js
@@ -1,5 +1,11 @@
 function auth() {
-  console.log("hi");
+  ensureAuthenticated = function(req, res, next) {
+    if (req.isAuthenticated()) { return next(); }
+    console.log("Please login to view this resource")
+    res.redirect("users/login");
+  }
 }
 
 export default auth;
+
+//unused currently
