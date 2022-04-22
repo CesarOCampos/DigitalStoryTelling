20682ef05a18b3c2485565f5e02493e9c12a21e9
Santos Garcia <89603557+JAIM307@users.noreply.github.com>
2022-04-21 21:28:06 -0500

    Updated route for the sign up page
    When you sign up, it will redirect you to the dashboard.

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link'
-import Footer from '../components/footer'
 import { useRouter } from 'next/router'
 import React, { useState } from 'react'
 import { Button, Form } from 'react-bootstrap'
